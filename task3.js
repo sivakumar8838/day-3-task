8a6ff28b4a93e50d8b5f64f4ccdd35921e220ddb
sivakumar8838 <142517633+sivakumar8838@users.noreply.github.com>
2023-08-22 12:39:03 +0530

    q3.js

--- task3.js
+++ task3.js
@@ -5,20 +5,16 @@ let a = new XMLHttpRequest();
 a.open ('GET', 'https://restcountries.com/v3.1/all')
 
 a.onload = function (){
-    let results = JSON.parse(a.responseText);
+    let x = JSON.parse(a.responseText);
  
    // let userNames =[];
 
-    for (let result of results){
-       // userNames.push(user['name']);
-
-       console.log(result.name.common);
-        console.log(result.region);
-        console.log(result.subregion);
-        console.log(result.population + "\n");
+    for (let i of x){
+        
+       console.log(i.name.common);
+        console.log(i.region);
+        console.log(i.subregion);
+        console.log(i.population + "\n");
     }
-     
-     
-    //console.log(userNames);
 }
 a.send();
